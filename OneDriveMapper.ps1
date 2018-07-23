5c2b186aa9aa6ea563122f16545121dfc5b7f538
Jos Lieben <jos.lieben@ogd.nl>
2018-07-23 15:18:29 +0200

    Microsoft changed favorite site listing, fix deployed

--- OneDriveMapper.ps1
+++ OneDriveMapper.ps1
@@ -3307,18 +3307,23 @@ if($autoMapFavoriteSites){
     if($authMethod -eq "native"){
         try{
             log -text "Retrieving favorited sites because autoMapFavoriteSites is set to TRUE"
-            $res = JosL-WebRequest -url $favoritesURL -Method GET
+            $customHeaders = @{"Upgrade-Insecure-Requests" = 1;"Cache-Control" = "max-age=0";"Accept-Language"="en-US,en;q=0.9,nl;q=0.8"}
+            $res = JosL-WebRequest -url $favoritesURL -Method GET -accept "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -customHeaders $customHeaders
         }catch{
             log -text "error retrieving favorited sites $($Error[0])" -fout
         }
         $res = handleSpoReAuth -res $res        
         $accessToken = returnEnclosedFormValue -res $res -searchString "`"AccessToken`":`""
         $resource = returnEnclosedFormValue -res $res -searchString "`",`"Resource`":`""
-        $payLoad = (returnEnclosedFormValue -res $res -searchString "Payload`":`"{" -endString "}").Replace("\","")
-        $payLoad = "{$payLoad}"
-        $customHeaders = @{"SPHome-ApiContext" = $payLoad; "SPHome-MicroserviceFlights" = "SPHomeServiceChangeLog;SPHomeServicePersonalCache;SPHomeServiceOLSAsPrimary";"SPHome-ClientType" = "Web";"Authorization" = "Bearer $accessToken"} 
-        $res = JosL-WebRequest -body "" -url "$resource/api/v1/sites/followed?mostRecentFirst=true&start=0&count=100&fillSiteData=true" -method POST -customHeaders $customHeaders -contentType "application/json;odata=verbose" -accept "application/json;odata=verbose"
-        $results = ($res.Content | convertfrom-json).Items
+        if($accessToken -eq -1 -and $resource -eq -1){
+            log -text "Failed to retrieve favorited sites, mapping to these sites will fail" -fout
+        }else{
+            $payLoad = (returnEnclosedFormValue -res $res -searchString "Payload`":`"{" -endString "}").Replace("\","")
+            $payLoad = "{$payLoad}"
+            $customHeaders = @{"SPHome-ApiContext" = $payLoad; "SPHome-MicroserviceFlights" = "SPHomeServiceChangeLog;SPHomeServicePersonalCache;SPHomeServiceOLSAsPrimary";"SPHome-ClientType" = "Web";"Authorization" = "Bearer $accessToken"} 
+            $res = JosL-WebRequest -body "" -url "$resource/api/v1/sites/followed?mostRecentFirst=true&start=0&count=100&fillSiteData=true" -method POST -customHeaders $customHeaders -contentType "application/json;odata=verbose" -accept "application/json;odata=verbose"
+            $results = ($res.Content | convertfrom-json).Items
+        }
         foreach($result in $results){
             $desiredUrl = $result.Url.Replace("https://","\\").Replace("/_layouts/15/start.aspx#","").Replace("sharepoint.com/","sharepoint.com@SSL\DavWWWRoot\").Replace("/Forms/AllItems.aspx","").Replace("/","\")
             if($autoMapFavoritesMode -eq "Normal"){
